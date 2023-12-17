66d442c27a80da2e3ef94bb26f07bed70bbf3b74
Toshiki Itai <toshiki.itai@linecorp.com>
2023-12-17 23:21:15 +0900

    error catch

--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -9,53 +9,67 @@ type Quiz = {
 };
 
 export default async function Quiz({ params }: { params: { id: number } }) {
-  const quiz = (await fetchClient(`/api/quiz/${params.id}`, {
-    cache: "no-store",
-  })) as Quiz;
+  try {
+    const quiz = (await fetchClient(`/api/quiz/${params.id}`, {
+      cache: "no-store",
+    })) as Quiz;
 
-  return (
-    <Center h="100%">
-      {quiz.type === "CHOICE" && (
-        <Flex
-          direction="column"
-          justify="space-between"
-          style={{
-            backgroundColor: "#FFFFFF",
-            width: "90%",
-            height: "90%",
-            opacity: "85%",
-            borderRadius: "48px",
-            padding: "48px",
-            boxShadow: "0 10px 16px 0 rgba(0, 0, 0, .5)",
-          }}
-        >
-          <Box>
-            <Text size="48px" fw="bold">
-              Q{quiz.id}
-            </Text>
-            <Text size="48px" fw="bold">
-              {quiz.text}
-            </Text>{" "}
-          </Box>
-          <Grid>
-            {quiz.options.map((option, i) => (
-              <Box
-                key={option}
-                style={{
-                  width: "50%",
-                  padding: "16px",
-                }}
-              >
-                <Text size="48px" fw="bold">
-                  {i + 1} {option}
-                </Text>
-              </Box>
-            ))}
-          </Grid>
-        </Flex>
-      )}
-    </Center>
-  );
+    return (
+      <Center h="100%">
+        {quiz.type === "CHOICE" && (
+          <Flex
+            direction="column"
+            justify="space-between"
+            style={{
+              backgroundColor: "#FFFFFF",
+              width: "90%",
+              height: "90%",
+              opacity: "85%",
+              borderRadius: "48px",
+              padding: "48px",
+              boxShadow: "0 10px 16px 0 rgba(0, 0, 0, .5)",
+            }}
+          >
+            <Box>
+              <Text size="48px" fw="bold">
+                Q{quiz.id}
+              </Text>
+              <Text size="48px" fw="bold">
+                {quiz.text}
+              </Text>{" "}
+            </Box>
+            <Grid>
+              {quiz.options.map((option, i) => (
+                <Box
+                  key={option}
+                  style={{
+                    width: "50%",
+                    padding: "16px",
+                  }}
+                >
+                  <Text size="48px" fw="bold">
+                    {i + 1} {option}
+                  </Text>
+                </Box>
+              ))}
+            </Grid>
+          </Flex>
+        )}
+      </Center>
+    );
+
+  } catch (error: any) {
+    return (
+      <Center h="100%">
+        <Text size="48px" fw="bold">
+          500
+        </Text>
+        <Text size="48px" fw="bold">
+          {error.message}
+        </Text>
+      </Center>
+    );
+  }
 }
 
 export async function generateStaticParams() {
